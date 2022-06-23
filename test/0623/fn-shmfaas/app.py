b31d9691092799c43e59ce8dd38d535404eca9f5
freckie <freckie@frec.kr>
2022-06-23 17:18:24 +0900

    Add: 0623 test on knative

---
+++ test/0623/fn-shmfaas/app.py
@@ -0,0 +1,37 @@
+import os
+
+from flask import Flask
+
+import json
+import numpy as np
+import torch
+import torchvision.models as models
+import pickle
+from PIL import Image
+
+import shmtorch
+
+app = Flask(__name__)
+
+@app.route('/')
+def predict():
+    model_skeleton = models.vgg16(False, False)
+
+    metadata: shmtorch.XMetadata
+    with open('/app/metadata', 'rb') as f:
+        metadata = pickle.load(f)
+
+    shm, model = shmtorch.x_load_states(model_skeleton, metadata)
+    model.eval()
+
+    del model_skeleton
+    del metadata
+
+    img = Image.open('/app/dog-224.jpg').convert('RGB')
+    t = torch.unsqueeze(torch.from_numpy(np.array(img)), 0)
+    result = model(t)
+
+    return json.dumps({'result': str(torch.argmax(result, dim=1))})
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
